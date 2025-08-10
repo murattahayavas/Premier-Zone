05b5d72c0e673dc022ecd109e628a3c6ccae23a3
taha <murattahayavas@gmail.com>
2025-08-10 18:12:26 +0300

    feat: add application.properties, PlayerController, and PremierZoneApplication

---
+++ src/main/java/com/tr/premier_zone/PremierZoneApplication.java
@@ -0,0 +1,13 @@
+package com.tr.premier_zone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremierZoneApplication.class, args);
+	}
+
+}
